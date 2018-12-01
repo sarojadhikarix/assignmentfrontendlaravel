70ea32825ae81c9691101936a5792fb2fbd662cb
DESKTOP-FD9LD0D\n0br41n <michael4720@msn.com>
2018-12-02 00:41:14 +0100

    Template   - Basic Nav   - Control Panel   - Layout Structure

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,5 @@
+<section id="content">
+
+  @yield('content')
+
+</section>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.head')
+
+<body>
+@include('layouts.nav')
+
+@include('layouts.control_panel')
+
+@include('layouts.content')
+
+  <script src="{{asset('js/app.js')}}"></script>
+
+
+</body>
+</html>
